7ba5be43392b77e33c92e97181d1c06c4a27095f
DanielRabl <danielrabl1999@gmail.com>
2026-01-31 19:55:03 +0100

    fixed script-run.bat to use QL template

--- script-run.bat
+++ script-run.bat
@@ -1,12 +1,12 @@
 @echo off
-msbuild.exe Uzumaki.vcxproj /property:Configuration=Release /property:Platform=x64
+msbuild.exe QL.vcxproj /property:Configuration=Release /property:Platform=x64
 
 if %ERRORLEVEL% EQU 0 (
 	echo [90mbuild succeeded, running...
 	echo [97m
 	
-	xcopy /s /y .\x64\Release\Uzumaki.exe .\Uzumaki.exe
-	.\Uzumaki.exe
+	xcopy /s /y .\x64\Release\QL.exe .\QL.exe
+	.\QL.exe
 ) else (
 	echo [91mBuild failed!
 	echo:
